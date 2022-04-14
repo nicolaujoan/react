c37d67cadb48ef0be74164c27176ee052bbb1f7a
nicolaujoan <jnicolausantandreu@cifpfb.eu>
2022-04-14 19:21:20 +0200

    add state for year and manage lifting the state up

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,16 +1,35 @@
-import './Expenses.css';
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
+import Card from "../UI/Card";
+import { useState } from "react";
 
-const Expenses = props => {
-    return <Card className='expenses-list'>
-        <ExpenseItem title={props.expenses[0].title}
-        amount={props.expenses[0].amount}></ExpenseItem>
-        <ExpenseItem title={props.expenses[1].title}
-        amount={props.expenses[1].amount}></ExpenseItem>
-        <ExpenseItem title={props.expenses[2].title}
-        amount={props.expenses[2].amount}></ExpenseItem>
-    </Card>
-}
+const Expenses = (props) => {
+  const [year, setYear] = useState(2022);
+
+  const changeYearHandler = (selectedYear) => {
+    setYear(selectedYear);
+  };
+
+  return (
+    <div>
+      <Card className="expenses-list">
+        <ExpenseFilter onChangeYear={changeYearHandler} />
+        <ExpenseItem
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+        ></ExpenseItem>
+      </Card>
+    </div>
+  );
+};
 
 export default Expenses;
